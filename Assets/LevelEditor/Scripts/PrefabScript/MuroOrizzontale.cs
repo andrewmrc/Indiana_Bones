d089b1d44cce17a480c36d7cfa678e141f660a3c
dbga_26 <dbga_26@dbga.local>
2016-07-13 17:59:26 +0200

    Grandezza Griglia Valori Fixed (quindi la grandezza della griglia si può modificare nell'inspector senza errori) Inizio ottimizzazioni dei Prefab

--- Assets/LevelEditor/Scripts/PrefabScript/MuroOrizzontale.cs
+++ Assets/LevelEditor/Scripts/PrefabScript/MuroOrizzontale.cs
@@ -8,21 +8,20 @@ namespace IndianaBones
     {
         int x;
         int y;
-        
+
         void Start()
         {
             Grid elementi = FindObjectOfType<Grid>();
             x = (int)this.transform.position.x;
             y = (int)this.transform.position.y;
             elementi.scacchiera[x, y].status = 2;
-            SpriteRenderer muro = elementi.scacchiera[x, y].GetComponent<SpriteRenderer>();
-            muro.sprite = Resources.Load("muroOrizzontale", typeof(Sprite)) as Sprite;
-            this.transform.position = new Vector2(x, y);
-            elementi.scacchiera[x, y].name = "Muro_Orizzontale";
+
+
             elementi.scacchiera[x, y].gameObject.AddComponent<BoxCollider2D>();
-            Destroy(this.gameObject);
+
 
         }
+
 
     }
 }
