b8405ef702403a1475a3a0f5e4fad4fb752423fa
dbga_26 <dbga_26@dbga.local>
2016-07-15 10:17:35 +0200

    Aggiunto Script Buco Nero

---
+++ Assets/LevelEditor/Scripts/PrefabScript/BucoNero.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IndianaBones
+{
+
+    public class BucoNero : MonoBehaviour
+    {
+        int x;
+        int y;
+
+        void Start()
+        {
+            Grid elementi = FindObjectOfType<Grid>();
+            x = (int)this.transform.position.x;
+            y = (int)this.transform.position.y;
+            elementi.scacchiera[x, y].status = -1;
+            elementi.scacchiera[x, y].name = "buconero";
+            SpriteRenderer nero = elementi.scacchiera[x, y].GetComponent<SpriteRenderer>();
+            nero.sprite = Resources.Load("nera", typeof(Sprite)) as Sprite;
+            Destroy(this.gameObject);
+            
+
+
+        }
+
+    }
+}
