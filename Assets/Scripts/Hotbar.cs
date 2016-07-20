6e21cb73e78f390985eb091c4862417bed3a0800
dbga_04 <dbga_04@dbga.local>
2016-07-20 12:51:37 +0200

    - Inseriti asset grafici definitivi nella cartella: Asset_Grafici_Definitivi - Iniziato a preparare le sprite per le animazioni - Copiati nella scena Livello 1 tutti i componenti necessari a far funzionare il Player in modo da testare il livello.

--- Assets/Scripts/Hotbar.cs
+++ Assets/Scripts/Hotbar.cs
@@ -1,10 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-
 using System.Collections;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,44 +11,7 @@ public class Hotbar : MonoBehaviour
 	public KeyCode[] keyCodesForSlots = new KeyCode[8];
 	[SerializeField]
 	public int slotsInTotal;
-	/*
-	#if UNITY_EDITOR
-	[MenuItem("Master System/Create/Hotbar")]        //creating the menu item
-	public static void menuItemCreateInventory()       //create the inventory at start
-	{
-		GameObject Canvas = null;
-		if (GameObject.FindGameObjectWithTag("Canvas") == null)
-		{
-			GameObject inventory = new GameObject();
-			inventory.name = "Inventories";
-			Canvas = (GameObject)Instantiate(Resources.Load("Prefabs/Canvas - Inventory") as GameObject);
-			Canvas.transform.SetParent(inventory.transform, true);
-			GameObject panel = (GameObject)Instantiate(Resources.Load("Prefabs/Panel - Hotbar") as GameObject);
-			panel.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
-			panel.transform.SetParent(Canvas.transform, true);
-			GameObject draggingItem = (GameObject)Instantiate(Resources.Load("Prefabs/DraggingItem") as GameObject);
-			Instantiate(Resources.Load("Prefabs/EventSystem") as GameObject);
-			draggingItem.transform.SetParent(Canvas.transform, true);
-			Inventory inv = panel.AddComponent<Inventory>();
-			panel.AddComponent<InventoryDesign>();
-			panel.AddComponent<Hotbar>();
-			inv.getPrefabs();
-		}
-		else
-		{
-			GameObject panel = (GameObject)Instantiate(Resources.Load("Prefabs/Panel - Hotbar") as GameObject);
-			panel.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, true);
-			panel.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
-			Inventory inv = panel.AddComponent<Inventory>();
-			panel.AddComponent<Hotbar>();
-			DestroyImmediate(GameObject.FindGameObjectWithTag("DraggingItem"));
-			GameObject draggingItem = (GameObject)Instantiate(Resources.Load("Prefabs/DraggingItem") as GameObject);
-			draggingItem.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, true);
-			panel.AddComponent<InventoryDesign>();
-			inv.getPrefabs();
-		}
-	}
-	#endif*/
+
 
 	void Start()
 	{
@@ -64,15 +23,7 @@ public class Hotbar : MonoBehaviour
 		for (int i = 0; i < slotsInTotal; i++)
 		{
 			if (Input.GetKeyDown(keyCodesForSlots[i]))
-			{/*
-				if (transform.GetChild(1).GetChild(i).childCount != 0 && transform.GetChild(1).GetChild(i).GetChild(0).GetComponent<ItemOnObject>().item.itemType != ItemType.UFPS_Ammo)
-				{
-					if (transform.GetChild(1).GetChild(i).GetChild(0).GetComponent<ConsumeItem>().duplication != null && transform.GetChild(1).GetChild(i).GetChild(0).GetComponent<ItemOnObject>().item.maxStack == 1)
-					{
-						Destroy(transform.GetChild(1).GetChild(i).GetChild(0).GetComponent<ConsumeItem>().duplication);
-					}
-					transform.GetChild(1).GetChild(i).GetChild(0).GetComponent<ConsumeItem>().consumeIt();
-				}*/
+			{
 				if (transform.GetChild (i).childCount != 0) {
 					transform.GetChild (i).GetComponent<Image> ().color = new Color32 (0, 160, 255, 255);
 					transform.GetChild (i).GetComponent<Button> ().onClick.Invoke();
@@ -88,8 +39,6 @@ public class Hotbar : MonoBehaviour
 
 	public int getSlotsInTotal()
 	{
-		//Inventory inv = GetComponent<Inventory>();
-		//return slotsInTotal = inv.width * inv.height;
 		return slotsInTotal = transform.childCount;
 	}
 }
