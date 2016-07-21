1604614a7ee0f22c9bbd8cf6717123b2099e9a6d
dbga_26 <dbga_26@dbga.local>
2016-07-21 17:47:05 +0200

    Implementazione della meccanica del Main Menu : - creazione dei 3 pulsanti principali controllabili da tastiera e funzionanti  ("New Game" (che porta alla Scenaprova) , "Credits" ed "Exit") - creazione della scena in cui andranno i Credits con pulsante "Return" per tornare al Main Menu

---
+++ Assets/Menu/buttons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+    public Button butto;
+
+    void Start()
+    {
+        //butto.Select();
+    }
+	public void goToNextLevel()
+    {
+        SceneManager.LoadScene("ScenaProva");
+    }
+    public void exitToWindows()
+    {
+        Application.Quit();
+    }
+    public void goToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void goToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
