7571cfa03c29646999c5af70e7bf1cd538b1f2d6
Daniele Sangineto <scidros@gmail.com>
2016-07-23 17:52:45 +0200

    Aggiunti i 10 Prefab della Piscina di Cleopatra con relativo script : - ogni prefab contiene un valore pubblico di danno al Player - il Player quando sosta in uno dei prefab che compongono la piscina subisce un danno pari al valore di "attacco" impostato nel Prefab

---
+++ Assets/LevelEditor/Scripts/PrefabScript/Piscina.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IndianaBones
+{
+
+    public class Piscina : MonoBehaviour
+    {
+       
+        int x;
+        int y;
+
+        public int DannoPiscina = 1;
+        bool attiva = true;
+        void Start()
+        {
+
+
+           
+
+            x = (int)this.transform.position.x;
+            y = (int)this.transform.position.y;
+
+            Grid elementi = FindObjectOfType<Grid>();
+            this.transform.position = elementi.scacchiera[x, y].transform.position;
+        }
+
+
+        IEnumerator ResetPlayerColor()
+        {
+            yield return new WaitForSeconds(0.3f);
+            Player.Self.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+
+        }
+
+
+        void Update()
+        {
+            
+
+            Grid elementi = FindObjectOfType<Grid>();
+            
+            if (attiva == true && Player.Self.transform.position == elementi.scacchiera[x, y].transform.position)
+            {
+                attiva = false;
+
+                Player.Self.gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+                StartCoroutine(ResetPlayerColor());
+
+                Player.Self.currentLife -= DannoPiscina;
+
+
+            }
+
+            else if (Player.Self.transform.position != elementi.scacchiera[x, y].transform.position)
+                attiva = true;
+        }
+
+    }
+}
