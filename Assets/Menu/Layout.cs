a50cb4960b05d3d2eee43fbbf0e4fa6b4ce9d0e3
dbga_26 <dbga_26@dbga.local>
2016-07-25 16:08:44 +0200

    Menu con grafica definitiva per quanto concerne i pulsanti e semi definitva (da sostituire con gli asset definitvi non appena saranno disponibili) per quanto riguarda lo sfondo del Main Menu e dei Credits

---
+++ Assets/Menu/Layout.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layout : MonoBehaviour {
+
+    private int screenX = 1024;
+    private int screenY = 768;
+
+    void Awake()
+    {
+        TweakLayout();
+    }
+
+    void Update()
+    {
+        if (screenX != Screen.width || screenY != Screen.height)
+        {
+            screenX = Screen.width;
+            screenY = Screen.height;
+
+            TweakLayout();
+        }
+    }
+
+    void TweakLayout()
+    {
+        float currentRatio = (float)screenX / screenY;
+        if (currentRatio <= 1.555f)
+        {
+            // tweak UI
+        }
+        else
+        {
+            // tweak UI
+        }
+    }
+}
