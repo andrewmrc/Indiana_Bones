f1c746b89c3980a26f3d87bc3573d266503224b1
dbga_26 <dbga_26@dbga.local>
2016-07-27 16:18:19 +0200

    - Inserimento Prefab del vaso con variabile pubblica personalizzabile

---
+++ Assets/LevelEditor/Scripts/PrefabScript/Vaso.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IndianaBones
+{
+
+    public class Vaso : MonoBehaviour
+    {
+        int x;
+        int y;
+
+        public int DannoPerDistruggerlo = 1;
+        Grid elementi;
+
+        void Start()
+        {
+            elementi = FindObjectOfType<Grid>();
+            x = (int)this.transform.position.x;
+            y = (int)this.transform.position.y;
+            elementi.scacchiera[x, y].status = 3;
+            elementi.scacchiera[x, y].name = "vaso";
+
+
+            
+
+
+        }
+
+        public void OnTriggerEnter2D(Collider2D coll)
+        {
+
+            //Handle life subtraction
+            if (coll.gameObject.name == "up")
+            {
+                if (Player.Self.croce == 1)
+                {
+
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+                    StartCoroutine(ResetMyColor());
+                    DannoPerDistruggerlo -= Player.Self.currentAttack;
+
+                }
+            }
+            if (coll.gameObject.name == "down")
+            {
+                if (Player.Self.croce == 3)
+                {
+
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+                    StartCoroutine(ResetMyColor());
+                    DannoPerDistruggerlo -= Player.Self.currentAttack;
+
+                }
+            }
+            if (coll.gameObject.name == "right")
+            {
+                if (Player.Self.croce == 2)
+                {
+
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+                    StartCoroutine(ResetMyColor());
+                    DannoPerDistruggerlo -= Player.Self.currentAttack;
+
+                }
+            }
+            if (coll.gameObject.name == "left")
+            {
+                if (Player.Self.croce == 4)
+                {
+
+
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+                    StartCoroutine(ResetMyColor());
+                    DannoPerDistruggerlo -= Player.Self.currentAttack;
+
+                }
+            }
+
+
+        }
+
+        IEnumerator ResetMyColor()
+        {
+            yield return new WaitForSeconds(0.2f);
+            gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        }
+
+        void Update()
+        {
+            if (DannoPerDistruggerlo <= 0)
+            {
+                elementi.scacchiera[x, y].status = 0;
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+}
