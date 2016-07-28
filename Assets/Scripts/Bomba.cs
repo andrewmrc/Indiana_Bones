23f02a9583d143b24c5e0d785a7fd8d1829ed7a3
dbga_26 <dbga_26@dbga.local>
2016-07-28 18:01:24 +0200

    -Creati gli script di Bomba e Veleno ed agganciati ai prefab : - il danno che fa la bomba in fase di attivazione del collider va definito nei nemici - il danno che fa il veleno in fase di attivazione del collider va definito nei nemici - la turnazione del veleno va sistemata (dopo 3 turni deve distruggersi)

---
+++ Assets/Scripts/Bomba.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IndianaBones
+{
+
+    public class Bomba : Character {
+
+        Grid elementi;
+        Rigidbody2D rb;
+        BoxCollider2D bc;
+        
+
+        float forza = 0.1f;
+        bool seen = false;
+
+        void Start () {
+
+            elementi = FindObjectOfType<Grid>();
+            rb = GetComponent<Rigidbody2D>();
+            bc = GetComponent<BoxCollider2D>();
+
+        }
+
+        void FixedUpdate()
+        {
+             
+
+            Player objPlayer = FindObjectOfType<Player>();
+
+            switch (objPlayer.bulletDir)
+            {
+                case 1:
+                    rb.AddForce(transform.up * forza, ForceMode2D.Impulse);
+                    StartCoroutine("DestroyBomb");
+                    break;
+                case 2:
+                    rb.AddForce(transform.up * -forza, ForceMode2D.Impulse);
+                    StartCoroutine("DestroyBomb");
+                    break;
+                case 3:
+                    rb.AddForce(transform.right * forza, ForceMode2D.Impulse);
+                    StartCoroutine("DestroyBomb");
+                    break;
+                case 4:
+                    rb.AddForce(transform.right * -forza, ForceMode2D.Impulse);
+                    StartCoroutine("DestroyBomb");
+                    break;
+            }
+
+
+        }
+
+       
+    IEnumerator DestroyBomb()
+        {
+        yield return new WaitForSeconds(0.85f);
+        
+            rb.drag = 500;
+            bc.enabled = true;
+
+            yield return new WaitForSeconds(0.4f);
+            GameController.Self.PassTurn();
+            Destroy(this.gameObject);
+            Debug.LogWarning("sono qui");
+        }
+
+
+        public void OnCollisionEnter2D(Collision2D coll)
+        {
+
+            if (coll.gameObject.tag == "Walls" || coll.gameObject.tag == "Colonne")
+            {
+                
+                GameController.Self.PassTurn();
+                Player.Self.ResetPlayerVar();
+
+                Destroy(this.gameObject);
+
+            }
+           
+        }
+
+        void Update () {
+
+           
+        }
+}
+}
---
+++ Assets/Scripts/Veleno.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace IndianaBones
+{
+
+    public class Veleno : Character {
+
+        Grid elementi;
+        Rigidbody2D rb;
+        BoxCollider2D bc;
+
+        bool attivo = true;
+        int turnoVeleno = 0; 
+
+        float forza = 0.1f;
+        bool seen = false;
+
+        void Start () {
+
+            elementi = FindObjectOfType<Grid>();
+            rb = GetComponent<Rigidbody2D>();
+            bc = GetComponent<BoxCollider2D>();
+
+        }
+
+        void FixedUpdate()
+        {
+             
+
+            Player objPlayer = FindObjectOfType<Player>();
+
+            if (attivo == true)
+            {
+                switch (objPlayer.bulletDir)
+                {
+                    case 1:
+                        rb.AddForce(transform.up * forza, ForceMode2D.Impulse);
+                        StartCoroutine("LancioVeleno");
+                        break;
+                    case 2:
+                        rb.AddForce(transform.up * -forza, ForceMode2D.Impulse);
+                        StartCoroutine("LancioVeleno");
+                        break;
+                    case 3:
+                        rb.AddForce(transform.right * forza, ForceMode2D.Impulse);
+                        StartCoroutine("LancioVeleno");
+                        break;
+                    case 4:
+                        rb.AddForce(transform.right * -forza, ForceMode2D.Impulse);
+                        StartCoroutine("LancioVeleno");
+                        break;
+                }
+            }
+
+        }
+
+       
+    IEnumerator LancioVeleno()
+        {
+        yield return new WaitForSeconds(0.85f);
+        
+            rb.drag = 500;
+            bc.enabled = true;
+            turnoVeleno = 1;
+            attivo = false;
+
+            
+        }
+
+
+        public void OnCollisionEnter2D(Collision2D coll)
+        {
+
+            if (coll.gameObject.tag == "Walls" || coll.gameObject.tag == "Colonne")
+            {
+                
+                GameController.Self.PassTurn();
+                
+                Destroy(this.gameObject);
+                
+
+            }
+           
+        }
+
+        void Update()
+        {
+
+            if (turnoVeleno < 3 && gameObject.GetComponent<TurnHandler>().itsMyTurn)
+            {
+                
+                turnoVeleno += 1;
+                GameController.Self.PassTurn();
+                Debug.LogWarning("eccomi");
+
+            }
+            else if ( turnoVeleno >= 3)
+            {
+                GameController.Self.PassTurn();
+                Destroy(this.gameObject);
+
+            }
+
+
+        }
+}
+}
