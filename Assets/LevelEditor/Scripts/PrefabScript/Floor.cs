4ab3a9435b5ab09c87977a32f8a41441b1c90bc5
dbga_74 <dbga_74@dbga.local>
2016-07-28 17:24:00 +0200

    -Aggiornate tile di tutti i livelli -Aggiornato script griglia per rotazione pavimenti con ombre

--- Assets/LevelEditor/Scripts/PrefabScript/Floor.cs
+++ Assets/LevelEditor/Scripts/PrefabScript/Floor.cs
@@ -8,19 +8,23 @@ namespace IndianaBones
     {
         int x;
         int y;
-		string tileName;
+        int z;
+        string tileName;
 
         void Start()
         {
             Grid myGrid = FindObjectOfType<Grid>();
             x = (int)this.transform.position.x;
             y = (int)this.transform.position.y;
+            z = (int)this.transform.rotation.eulerAngles.z;
+            Debug.Log("la z di " + this.gameObject.name + " è: " +z);
 			tileName = this.gameObject.GetComponent<SpriteRenderer>().sprite.name;
 			Debug.Log ("Tile Name: " + tileName);
             SpriteRenderer pavimento = myGrid.scacchiera[x, y].GetComponent<SpriteRenderer>();
 			pavimento.sprite = Resources.Load(tileName, typeof(Sprite)) as Sprite;
             this.transform.position = new Vector2(x, y);
-			myGrid.scacchiera[x, y].name = tileName;
+            myGrid.scacchiera[x, y].transform.rotation = Quaternion.Euler(0, 0, z);
+            myGrid.scacchiera[x, y].name = tileName;
             Destroy(this.gameObject);
 
         }
