9ca07cecb45c8db69f0405e93dc8d9cbb0d025e3
Andrea Mercuri <andrea.mercuri21@gmail.com>
2016-08-02 00:59:04 +0200

    - Dialogo livello due aggiunto - Scenetta recupero abilità funziona e non si ripete - Aggiornato script del trigger dialogue per permettere di disattivare oggetti di scena

--- Assets/Scripts/Dialogues/TriggerDialogue.cs
+++ Assets/Scripts/Dialogues/TriggerDialogue.cs
@@ -12,10 +12,15 @@ namespace IndianaBones
 		public GameObject dialogueToEnable;
 		public int dialogueIndex;
 		bool started;
+		public GameObject specialObjects;
 
 		// Use this for initialization
 		void Start () {
-		
+			if (DialoguesManager.Self.dialoguesActivated [dialogueIndex]) {
+				if (specialObjects != null) {
+					specialObjects.SetActive (false);
+				}
+			}
 		}
 		
 		// Update is called once per frame
@@ -27,11 +32,14 @@ namespace IndianaBones
 		{
 			if (coll.gameObject.name == "Player" && !Player.Self.endMove && !started) {
 				started = true;
-				if(Player.Self.GetComponent<DialoguesManager>().CheckSeen(dialogueIndex) == false){
+				if(DialoguesManager.Self.CheckSeen(dialogueIndex) == false){
 					Debug.Log ("Start Dialogue: " + DialoguesManager.Self.CheckSeen(dialogueIndex));
 					DialoguesManager.Self.SetDialogueSeen (dialogueIndex);
 					dialogueToEnable.SetActive (true);
 					//Destroy (this.gameObject);
+					if (specialObjects != null) {
+						specialObjects.SetActive (false);
+					}
 				}
 				this.gameObject.SetActive (false);
 			}
